af80e3420951f20def387c5ac4b0f4a66798b0d5
Scott Flicker <sflicker2@earthlink.net>
2025-04-27 21:22:16 -0400

    forking to create stage3 which will add equality expressions

---
+++ stage3_equality_expr/run_all_tests.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+set -e
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+PROG=$1
+
+echo "Cleaning previous runs..."
+rm -f tests/*.s tests/*.o
+
+
+for bin in tests/test*.c; do
+    base="${cfile%.*}"
+    if [ =f "$base" ]; then
+        echo "Deleting binary $base"
+        rm -f "$base"
+    fi
+done
+
+echo "Running all tests..."
+
+total_tests=0
+passed_tests=0
+failed_tests=0
+
+for cfile in tests/test*.c; do
+    total_tests=$((total_tests + 1))
+    echo "----------------------------"
+    echo "🔍 Running test for $cfile..."
+    
+    # assuming all tests should have an exit code of 42 to pass for now
+    if ./run_test.sh "$cfile" 42 $PROG; then
+        echo -e "${GREEN}✅ $cfile passed${NC}"
+        passed_tests=$((passed_tests + 1))
+    else
+        echo -e "${RED}❌ $cfile failed${NC}"
+        failed_tests=$((failed_tests + 1))
+    fi
+
+done
+
+echo "---------------------------------"
+echo "📝 Test Summary:"
+echo -e "${GREEN}✅ Passed: ${passed_tests}${NC}"
+echo -e "${RED}❌ Failed: ${failed_tests}${NC}"
+echo "🧮 Total tests: ${total_tests}"
+
+if [ "$failed_tests" -eq 0 ]; then
+    echo -e "${GREEN}🏆 All tests passed!${NC}"
+    exit 0
+else
+    echo -e "${RED}🔥 Some tests failed.${NC}"
+    exit 1
+fi
---
+++ stage3_equality_expr/run_test.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+SRC=$1
+EXPECTED=$2
+PROG=$3
+
+echo SRC=$SRC
+echo EXPECTED=$EXPECTED
+echo PROG=$PROG
+
+ASM_FILE="${SRC%.c}.s"
+OBJ_FILE="${SRC%.c}.o"
+EXE_FILE="${SRC%.*}"
+
+echo ASM_FILE=$ASM_FILE
+echo OBJ_FILE=$OBJ_FILE
+echo EXE_FILE=$EXE_FILE
+
+set -e
+
+./$PROG $SRC
+
+echo "linking..."
+nasm -f elf64 $ASM_FILE -o $OBJ_FILE
+gcc -no-pie -o $EXE_FILE $OBJ_FILE
+
+echo "executing compiled file..."
+set +e
+./"$EXE_FILE"
+EXIT_CODE=$?
+set -e
+echo "finished running compiled file"
+
+echo "Program '$EXE_FILE' exited with code $EXIT_CODE"
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+if [ $EXIT_CODE -eq $EXPECTED ]; then
+    echo -e "${GREEN}✅ Test: $SRC Passed${NC}"
+else 
+    echo -e "${RED}❌ Test: $SRC Failed: expected $EXPECTED, got $EXIT_CODE"
+    exit 1
+fi
