959a35ab80ada29386266b54e0dc4b40344eb0e0
Scott Flicker <sflicker2@earthlink.net>
2025-05-10 10:51:22 -0400

    adding stage8

---
+++ stage8_block_scope/util.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "util.h"
+
+char * my_strdup(const char* s) {
+    size_t len = strlen(s) + 1;
+    char * copy = malloc(len);
+    if (copy) {
+        memcpy(copy, s, len);
+    }
+    return copy;
+}
+
+void error(const char * message) {
+    fprintf(stderr, "%s\n", message);
+    exit(1);
+}
