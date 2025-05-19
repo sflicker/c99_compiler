b453b539aa203b8cc06f876a82d91f4ce48dbb2b
Scott Flicker <sflicker2@earthlink.net>
2025-05-19 08:59:17 -0400

    creating a TokenzierContext as part of Tokenzier changes

--- stage10_control_wrapup/tokenizer.c
+++ stage10_control_wrapup/tokenizer.c
@@ -21,58 +21,6 @@ Token * make_token(TokenType type, const char * text) {
     return token;
 }
 
-TokenizerContext * init_tokenizer_context(const char * text) {
-    TokenizerContext * context = malloc(sizeof(TokenizerContext));
-    context->text = text;
-    context->pos = 0;
-    context->line = 1;
-    context->col = 1;
-    context->current = context->text[context->pos];
-    return context;
-}
-
-char advance(TokenizerContext * context) {
-    context->pos++;
-    context->current = context->text[context->pos];
-    if (context->current == '\n') {
-        context->line++;
-        context->col=1;
-    }
-    else {
-        context->col++;
-    }
-    return context->current;
-}
-
-TokenList * get_tokens(TokenizerContext * context) {
-    TokenList * tokenList = malloc(sizeof(TokenList));
-
-    while(context->current) {
-        Token * token = get_next_token(context);
-        append_token(tokenList, token);
-    }
-
-    Token * eof = make_token(TOKEN_EOF, "");
-    append_token(tokenList, eof);
-    return tokenList;
-}
-
-Token * get_next_token(TokenizerContext * context) {
-    while(context->current) {
-        if (isspace(context->current)) {
-            advance(context);
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
 void init_token_list(TokenList * list);
 
 void init_token_list(TokenList * list) {
--- stage10_control_wrapup/tokenizer.h
+++ stage10_control_wrapup/tokenizer.h
@@ -3,17 +3,7 @@
 
 #include "token.h"
 
-typedef struct {
-    const char * text;
-    int pos;
-    char current;
-    int line;
-    int col;
-} TokenizerContext;
 
-TokenizerContext * init_tokenizer_context(const char * text);
-TokenList * get_tokens(TokenizerContext * context);
-Token * get_next_token(TokenizerContext * context);
 
 void cleanup_token_list(TokenList * tokenList);
 void tokenize(const char* code, TokenList * tokenList);
