e4ff084c2a60119e7d6f942ade21035b5156912f
Scott Flicker <sflicker2@earthlink.net>
2025-06-17 08:59:24 -0400

    work in progress

--- stage11_int_types/unit_tests/test_analyzer.c
+++ stage11_int_types/unit_tests/test_analyzer.c
@@ -31,20 +31,11 @@ void test_analyze_basic_case() {
 }
 
 void test_analyze_mixed_types() {
-    // const char * program = "int main() {\n"
-    //                        "    int a=1;\n"
-    //                        "    char b=2;\n"
-    //                        "    short c=3;\n"
-    //                        "    long d=4;\n"
-    //                        "    return a+b+c+d;\n"
-    //                        "}\n";
+
     const char * program = "int main() {\n"
                        "    int a=1;\n"
                        "    char b=2;\n"
                        "    return a+b;"
-                       // "    short c=3;\n"
-                       // "    long d=4;\n"
-                       // "    return a+b+c+d;\n"
                        "}\n";
 
     TEST_MSG("Input Source Program:");
@@ -61,8 +52,31 @@ void test_analyze_mixed_types() {
     print_ast(actual, 0);
 }
 
+void test_analyze_multi_mixed_types() {
+    const char * program = "int main() {\n"
+                           "    int a=1;\n"
+                           "    char b=2;\n"
+                           "    short c=3;\n"
+                           "    long d=4;\n"
+                           "    return a+b+c+d;\n"
+                           "}\n";
+
+    TEST_MSG("Input Source Program:");
+    TEST_MSG(program);
+
+    tokenlist * tokens = tokenize(program);
+
+    ASTNode * actual = parse(tokens);
+    print_ast(actual, 0);
+    init_symbol_table();
+    AnalyzerContext * context = analyzer_context_new();
+    context->current_function_return_type = &CTYPE_INT_T;
+    analyze(context, actual);
+    print_ast(actual, 0);
+}
 
 int main() {
-//    RUN_TEST(test_analyze_basic_case);
+    RUN_TEST(test_analyze_basic_case);
     RUN_TEST(test_analyze_mixed_types);
+    RUN_TEST(test_analyze_multi_mixed_types);
 }
