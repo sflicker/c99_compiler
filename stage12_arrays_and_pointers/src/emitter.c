4a0255dade5c33686e0aa2f8f6bd732a847c8bef
Scott Flicker <sflicker2@earthlink.net>
2025-07-06 16:51:43 -0400

    work in progress

--- stage12_arrays_and_pointers/src/emitter.c
+++ stage12_arrays_and_pointers/src/emitter.c
@@ -800,9 +800,6 @@ void emit_var_declaration(EmitterContext * ctx, ASTNode * node) {
 }
 
 void emit_assignment(EmitterContext * ctx, ASTNode* node) {
-    // emit_tree_node(ctx, node->binary.rhs);
-    // char * reference_label = create_variable_reference(ctx, node->binary.lhs);
-    // emit_line(ctx, "mov %s, eax\n", reference_label);
     emit_expr(ctx, node->binary.rhs);
     emit_line(ctx, "push rax\n");
     emit_addr(ctx, node->binary.lhs);
