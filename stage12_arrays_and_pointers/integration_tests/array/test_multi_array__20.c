1aae39fe52e3cbe09c66a8ad496e493ad5cab069
Scott Flicker <sflicker2@earthlink.net>
2025-07-25 09:04:38 -0400

    adjusted integration test input back to original

--- stage12_arrays_and_pointers/integration_tests/array/test_multi_array__20.c
+++ stage12_arrays_and_pointers/integration_tests/array/test_multi_array__20.c
@@ -8,24 +8,17 @@ int i;
 int j;
 
 int main() {
-    int a[3][4];
-    for (i = 0; i < 3; i++) {
+    int a[5][4];
+    for (i = 0; i < 5; i++) {
         for (j = 0; j < 4; j++) {
-//            _print(i);
-//            _print(j);
             a[i][j] = 1;
         }
     }
     sum = 0;
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < 5; i++) {
         for (j = 0; j < 4; j++) {
-//            _print(i);
-//            _print(j);
             sum += a[i][j];
-//            _print(sum);
         }
     }
-//    _print(-256);
-//    _print(sum);
     return sum;   // 20
 }
