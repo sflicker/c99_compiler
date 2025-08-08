176d07717544fa034d528ac7eadc5d3fd263e975
Scott Flicker <sflicker2@earthlink.net>
2025-08-08 08:28:41 -0400

    updated test case so unused used function return is assigned to a variable.

--- stage13_string_literals/integration_tests/pointer/test_pointer_in_function_call__42.c
+++ stage13_string_literals/integration_tests/pointer/test_pointer_in_function_call__42.c
@@ -4,12 +4,12 @@
 
 int func(int *b) {
     *b = 42;
-    return 0;
+    return 0;        // currently not supporting void functions so just return 0 and don't use it in caller
 }
 
 
 int main() {
     int a = 10;
-    func(&a);
+    int dummy = func(&a);      // assign return to a dummy variable which will be ignored.
     return a;
 }
