57ec2b23e16e9c4533297784c0b3b8a115828c6d
Scott Flicker <sflicker2@earthlink.net>
2025-08-12 09:05:24 -0400

    work in progress

--- stage14_floating_point/include/string_builder.h
+++ stage14_floating_point/include/string_builder.h
@@ -2,7 +2,21 @@
 // Created by scott on 8/12/25.
 //
 
-#ifndef MIMIC99_STAGE1_STRING_BUILDER_H
-#define MIMIC99_STAGE13_STRING_BUILDER_H
+#ifndef _STRING_BUILDER_H
+#define _STRING_BUILDER_H
+#include "list_util.h"
 
-#endif //MIMIC99_STAGE13_STRING_BUILDER_H
+DEFINE_LINKED_LIST(char, char_list);
+
+typedef struct String_Builder{
+    char_list* string;
+
+} String_Builder;
+String_Builder * create_string_builder();
+String_Builder * string_builder_init(String_Builder * string_builder,char_list string);
+String_Builder * append_string(String_Builder * string_builder,char * string);
+String_Builder * append_char(String_Builder * string_builder,char c);
+void destroy_string_builder(String_Builder * string_builder);
+char * string_builder_to_string(String_Builder * string_builder);
+
+#endif //_STRING_BUILDER_H
