6ea38f59cfa868b8f867b40486cda95c7bece2fd
Scott Flicker <sflicker2@earthlink.net>
2025-08-14 08:49:55 -0400

    adding a string_builder module

--- stage14_floating_point/include/string_builder.h
+++ stage14_floating_point/include/string_builder.h
@@ -4,19 +4,20 @@
 
 #ifndef _STRING_BUILDER_H
 #define _STRING_BUILDER_H
-#include "list_util.h"
+#include <stddef.h>
 
-DEFINE_LINKED_LIST(char, char_list);
+int DEFAULT_CAPACITY = 128;
 
 typedef struct String_Builder{
-    char_list* string;
-
+    char * buffer;
+    size_t length;
+    size_t capacity;
 } String_Builder;
+
 String_Builder * create_string_builder();
-String_Builder * string_builder_init(String_Builder * string_builder,char_list string);
 String_Builder * append_string(String_Builder * string_builder,char * string);
 String_Builder * append_char(String_Builder * string_builder,char c);
-void destroy_string_builder(String_Builder * string_builder);
-char * string_builder_to_string(String_Builder * string_builder);
+void free_string_builder(String_Builder * string_builder);
+char * build_string(String_Builder * string_builder);
 
 #endif //_STRING_BUILDER_H
---
+++ stage14_floating_point/src/string_builder.c
@@ -0,0 +1,52 @@
+
+#include "string_builder.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+String_Builder * create_string_builder() {
+    String_Builder * builder = (String_Builder *)malloc(sizeof(String_Builder));
+    builder->length = 0;
+    builder->capacity = DEFAULT_CAPACITY;
+    builder->buffer = (char *)malloc(builder->capacity);
+    memset(builder->buffer, 0, builder->capacity);
+    return builder;
+}
+
+String_Builder * grow_string_builder(String_Builder * builder) {
+    char * current = builder->buffer;
+    builder->capacity *= 2;
+    builder->buffer = (char *)malloc(builder->capacity);
+    memset(builder->buffer, 0, builder->capacity);
+    memcpy(builder->buffer, current, builder->length);
+    free(current);
+    return builder;
+}
+
+void free_string_builder(String_Builder * builder) {
+    free(builder->buffer);
+}
+
+String_Builder * append_string(String_Builder * string_builder,char * string) {
+    int new_length = string_builder->length + strlen(string);
+    if (new_length > string_builder->capacity) {
+        string_builder = grow_string_builder(string_builder);
+    }
+    memcpy(string_builder->buffer + string_builder->length, string, strlen(string));
+    string_builder->length = new_length;
+    return string_builder;
+}
+
+String_Builder * append_char(String_Builder * string_builder,char c) {
+    int new_length = string_builder->length + 1;
+    if (new_length > string_builder->capacity) {
+        string_builder = grow_string_builder(string_builder);
+    }
+    memcpy(string_builder->buffer + string_builder->length, &c, 1);
+    string_builder->length = new_length;
+    return string_builder;
+}
+
+char * build_string(String_Builder * string_builder) {
+    return strdup(string_builder->buffer);
+}
